3e50c8c4a7cfb0ad1948ba4ab0d253885cf3585e
Justin Levi Winter <justinlevi@users.noreply.github.com>
2021-03-17 17:27:48 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react';
+import { render } from '@testing-library/react'; // This import statement isn't being used so probably don't need it
 import React from 'react';
 import './App.css';
-import "./pokecard.tsx";
+
+import "./pokecard.tsx"; // Take a look at this import statement - which one of these doesn't look like the other? *hint* - no file extension when importing & one other thing...
 
 function App() {
   return <div>
